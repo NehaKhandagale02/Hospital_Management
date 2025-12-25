0a16f508c10c27d2b5424b599620dfe33cf884aa
neha <nehakhandagale3204@gmail.com>
2025-12-25 20:49:32 +0530

    Initial commit

--- src/main/java/com/management/hospital/controller/PatientController.java
+++ src/main/java/com/management/hospital/controller/PatientController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//Patient controller
 @RestController
 @RequestMapping("/patient")
 public class PatientController {
